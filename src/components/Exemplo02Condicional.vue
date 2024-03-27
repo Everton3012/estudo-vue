d9cb1f85db0b7104bd313e2492592653c5013796
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-27 14:52:37 -0300

    condicional v-else-if

--- src/components/Exemplo02Condicional.vue
+++ src/components/Exemplo02Condicional.vue
@@ -3,11 +3,16 @@
 // Variaveis
 
 let exibir = true;
+let media = 0.5;
 </script>
 
 <!-- HTML -->
 <template>
   <h1 v-if="exibir">Hello World!</h1>
+
+  <p v-if="media >= 7">Aprovado(a)</p>
+  <p v-else-if="media >= 5 ">Em exame</p>
+  <p v-else>Reprovado(a)</p>
 </template>
 
 <style scoped>
