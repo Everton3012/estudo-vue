0be8b7931fc61a1177d5fe0989801ac1041b7ad5
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-27 14:42:09 -0300

    estrutura convidional v-if

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <!-- SCRIPT -->
 <script setup>
 // Importação dos componentes
-import Exemplo01 from './components/Exemplo01Interpolacao.vue'
+import Exemplo01 from '@/components/Exemplo01Interpolacao.vue';
+import Exemplo02 from '@/components/Exemplo02Condicional.vue'
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo01/>
+  
+    <Exemplo01/>
+    <Exemplo02/>
+
 </template>
 
 <style scoped>
---
+++ src/components/Exemplo02Condicional.vue
@@ -0,0 +1,15 @@
+<!-- SCRIPT -->
+<script setup>
+// Variaveis
+
+let exibir = true;
+</script>
+
+<!-- HTML -->
+<template>
+  <h1 v-if="exibir">Hello World!</h1>
+</template>
+
+<style scoped>
+
+</style>
