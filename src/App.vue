01a0ae08c9882a277f839fb349606cd44242ad1b
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-27 14:33:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- SCRIPT -->
+<script setup>
+// Importação dos componentes
+import Exemplo01 from './components/Exemplo01Interpolacao.vue'
+</script>
+
+<!-- HTML -->
+<template>
+  <Exemplo01/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Exemplo01Interpolacao.vue
@@ -0,0 +1,16 @@
+<!-- SCRIPT -->
+<script setup>
+// Variaveis
+
+let nome = 'David ';
+let idade = 26;
+</script>
+
+<!-- HTML -->
+<template>
+  <p>{{ `Olá ${nome}, Sua idade é ${idade} anos`}}</p>
+</template>
+
+<style scoped>
+
+</style>
