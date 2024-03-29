24b80de88b87e3b70a011eb3522213de152fc68e
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-29 00:24:02 -0300

    @click

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import Exemplo03 from '@/components/Exemplo03Laco.vue';
 import Exemplo04 from '@/components/Exemplo04HTML.vue';
 import Exemplo05 from '@/components/Exemplo05Model.vue';
 import Exemplo06 from '@/components/Exemplo06Classe.vue';
+import Exemplo07 from '@/components/Exemplo07Click.vue';
 </script>
 
 <!-- HTML -->
@@ -18,6 +19,7 @@ import Exemplo06 from '@/components/Exemplo06Classe.vue';
     <Exemplo04/>
     <Exemplo05/>
     <Exemplo06/>
+    <Exemplo07/>
 </template>
 
 <style scoped>
---
+++ src/components/Exemplo07Click.vue
@@ -0,0 +1,23 @@
+<!-- SCRIPT -->
+<script setup>
+
+const mensagem = () => {
+  alert('Trabalhando com eventos de clique!');
+}
+</script>
+
+<!-- HTML -->
+<template>
+  <button @click="mensagem">Clique aqui</button>
+</template>
+
+<!-- CSS -->
+<style scoped>
+  .ok{
+    color:green;
+  }
+
+  .falha{
+    color:red;
+  }
+</style>
