c104d5de6ff477e141a4cacf2c6c1d6db2c98e11
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-29 19:47:43 -0300

    preventDefault

--- src/components/Exemplo09Enviar.vue
+++ src/components/Exemplo09Enviar.vue
@@ -1,14 +1,15 @@
 <!-- SCRIPT -->
 <script setup>
   //Funcao para validar formulario
-  const validarFormulario = () => {
+  const validarFormulario = (e) => {
     alert('Formulario enviado!');
+    e.preventDefault();
   }
 </script>
 
 <!-- HTML -->
 <template>
-  <form @submit="validarFormulario">
+  <form action="https://www.google.com" @submit="validarFormulario">
     <input type="submit" value="enviar">
   </form>
 </template>
